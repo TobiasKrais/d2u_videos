9cccc23195949f88fd71cd0f0b52d4bf9e2c4cd8
tobias <tobias@UbuntuXPS>
2018-06-15 22:06:21 +0200

    version 1.0.4

--- pages/setup.php
+++ pages/setup.php
@@ -33,6 +33,12 @@ $d2u_module_manager->showManagerList();
 <h2>Support</h2>
 <p>Fehlermeldungen bitte im <a href="https://github.com/TobiasKrais/d2u_videos" target="_blank">GitHub Repository</a> melden.</p>
 <h2>Changelog</h2>
+<p>1.0.4:</p>
+<ul>
+	<li>Zeigt playlist nur wenn mehr als ein Video vorhanden ist.</li>
+	<li>Bugfix: Speichern von Namen mit einfachem Anführungszeichen führte zu Fehler.</li>
+	<li>Bugfix: Änderungen in der Playlist wurde nicht richtig gespeichert.</li>
+</ul>
 <p>1.0.3:</p>
 <ul>
 	<li>Zeigt playlist nur wenn mehr als ein Video vorhanden ist.</li>
