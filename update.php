32f96aef0f1d22927c9ba620db3292795969aa23
tobias <tobias@UbuntuXPS>
2020-08-07 11:07:07 +0200

    wrong addon key

--- update.php
+++ update.php
@@ -5,7 +5,7 @@ if(class_exists('D2UModuleManager')) {
 	$modules[] = new D2UModule("30-1",
 		"D2U Videomanager - Video / Playlist",
 		1);
-	$d2u_module_manager = new D2UModuleManager($modules, "", "d2u_references");
+	$d2u_module_manager = new D2UModuleManager($modules, "", "d2u_videos");
 	$d2u_module_manager->autoupdate();
 }
 
